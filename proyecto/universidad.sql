be6c9e3e3b1106f8c8e760e08e6f16d442ec6477
EcuacionCuadratica.java <2019110470@udh.edu.pe>
2023-07-14 18:42:51 -0500

    mejora de base de datos

--- proyecto/universidad.sql
+++ proyecto/universidad.sql
@@ -1,114 +1,108 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: universidad
--- ------------------------------------------------------
--- Server version	8.0.30
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
---
--- Table structure for table `carreras`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema universidad
+-- -----------------------------------------------------
 
-DROP TABLE IF EXISTS `carreras`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `carreras` (
-  `id_carrera` int NOT NULL AUTO_INCREMENT,
-  `nombre_carrera` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_carrera`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Schema universidad
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `universidad` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `universidad` ;
 
---
--- Table structure for table `ciclos`
---
+-- -----------------------------------------------------
+-- Table `universidad`.`carreras`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidad`.`carreras` (
+  `id_carrera` INT NOT NULL AUTO_INCREMENT,
+  `nombre_carrera` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_carrera`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `ciclos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ciclos` (
-  `id_ciclo` int NOT NULL AUTO_INCREMENT,
-  `numero_ciclo` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_ciclo`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `cursos`
---
+-- -----------------------------------------------------
+-- Table `universidad`.`ciclos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidad`.`ciclos` (
+  `id_ciclo` INT NOT NULL AUTO_INCREMENT,
+  `numero_ciclo` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_ciclo`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `cursos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cursos` (
-  `id_curso` int NOT NULL AUTO_INCREMENT,
-  `nombre_curso` varchar(100) DEFAULT NULL,
-  `id_horario_curso` int DEFAULT NULL,
-  `id_carrera_curso` int DEFAULT NULL,
-  `id_ciclo_curso` int DEFAULT NULL,
-  `id_usuario_curso` int DEFAULT NULL,
+
+-- -----------------------------------------------------
+-- Table `universidad`.`cursos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidad`.`cursos` (
+  `id_curso` INT NOT NULL AUTO_INCREMENT,
+  `nombre_curso` VARCHAR(100) NULL DEFAULT NULL,
+  `id_horario_curso` INT NULL DEFAULT NULL,
+  `id_carrera_curso` INT NULL DEFAULT NULL,
+  `id_ciclo_curso` INT NULL DEFAULT NULL,
+  `id_usuario_curso` INT NULL DEFAULT NULL,
+  `usuarios_id` INT NOT NULL,
   PRIMARY KEY (`id_curso`),
-  KEY `FK_cursos_carreras` (`id_carrera_curso`),
-  KEY `FK_cursos_ciclos` (`id_ciclo_curso`),
-  CONSTRAINT `FK_cursos_carreras` FOREIGN KEY (`id_carrera_curso`) REFERENCES `carreras` (`id_carrera`),
-  CONSTRAINT `FK_cursos_ciclos` FOREIGN KEY (`id_ciclo_curso`) REFERENCES `ciclos` (`id_ciclo`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  INDEX `FK_cursos_carreras` (`id_carrera_curso` ASC) VISIBLE,
+  INDEX `FK_cursos_ciclos` (`id_ciclo_curso` ASC) VISIBLE,
+  CONSTRAINT `FK_cursos_carreras`
+    FOREIGN KEY (`id_carrera_curso`)
+    REFERENCES `universidad`.`carreras` (`id_carrera`),
+  CONSTRAINT `FK_cursos_ciclos`
+    FOREIGN KEY (`id_ciclo_curso`)
+    REFERENCES `universidad`.`ciclos` (`id_ciclo`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `estudiantes`
---
 
-DROP TABLE IF EXISTS `estudiantes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `estudiantes` (
-  `id_estudiante` int NOT NULL AUTO_INCREMENT,
-  `nombre_estudiante` varchar(100) DEFAULT NULL,
-  `apellido_estudiante` varchar(100) DEFAULT NULL,
-  `codigo_estudiante` int DEFAULT NULL,
-  `dni_estudiante` int DEFAULT NULL,
-  `contrasena_estudiante` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_estudiante`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Table `universidad`.`estudiantes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidad`.`estudiantes` (
+  `id_estudiante` INT NOT NULL AUTO_INCREMENT,
+  `nombre_estudiante` VARCHAR(100) NULL DEFAULT NULL,
+  `apellido_estudiante` VARCHAR(100) NULL DEFAULT NULL,
+  `codigo_estudiante` INT NULL DEFAULT NULL,
+  `dni_estudiante` INT NULL DEFAULT NULL,
+  `contrasena_estudiante` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_estudiante`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `matriculas`
---
 
-DROP TABLE IF EXISTS `matriculas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `matriculas` (
-  `id_matricula` int NOT NULL AUTO_INCREMENT,
-  `id_estudiante_matricula` int DEFAULT NULL,
-  `id_curso_matricula` int DEFAULT NULL,
+-- -----------------------------------------------------
+-- Table `universidad`.`matriculas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universidad`.`matriculas` (
+  `id_matricula` INT NOT NULL AUTO_INCREMENT,
+  `id_estudiante_matricula` INT NULL DEFAULT NULL,
+  `id_curso_matricula` INT NULL DEFAULT NULL,
   PRIMARY KEY (`id_matricula`),
-  KEY `FK_matriculas_estudiantes` (`id_estudiante_matricula`),
-  KEY `FK_matriculas_cursos` (`id_curso_matricula`),
-  CONSTRAINT `FK_matriculas_cursos` FOREIGN KEY (`id_curso_matricula`) REFERENCES `cursos` (`id_curso`),
-  CONSTRAINT `FK_matriculas_estudiantes` FOREIGN KEY (`id_estudiante_matricula`) REFERENCES `estudiantes` (`id_estudiante`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  INDEX `FK_matriculas_estudiantes` (`id_estudiante_matricula` ASC) VISIBLE,
+  INDEX `FK_matriculas_cursos` (`id_curso_matricula` ASC) VISIBLE,
+  CONSTRAINT `FK_matriculas_cursos`
+    FOREIGN KEY (`id_curso_matricula`)
+    REFERENCES `universidad`.`cursos` (`id_curso`),
+  CONSTRAINT `FK_matriculas_estudiantes`
+    FOREIGN KEY (`id_estudiante_matricula`)
+    REFERENCES `universidad`.`estudiantes` (`id_estudiante`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-14 18:09:06
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
